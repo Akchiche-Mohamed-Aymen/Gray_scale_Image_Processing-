63018a0dbc511f4271ed14beaac3fb1c99e56017
Akchiche-Mohamed-Aymen <akchiche.mohamedaymen@gmail.com>
2026-03-08 23:15:51 +0100

    creating processing.py file that has the logic side of operations

---
+++ processing.py
@@ -0,0 +1,41 @@
+from numpy import clip , uint8 , array , prod 
+def load_image(path = ''):
+    print(f'loading image from [ {path} ]')
+    img = array()
+    return img
+
+def adjust_brightness(img , beta):
+    temp_img = img + beta
+    temp_img = clip(temp_img , 0 , 255).astype(uint8)
+    return temp_img
+
+def linear_contrast(img , alpha , beta):
+    temp_img = alpha * img 
+    temp_img = adjust_brightness(temp_img , beta)
+    return temp_img
+
+def contrast_strech(img , f_min = None , f_max = None):
+    f_min = f_min if f_min is not None else img.min()
+    f_max = f_max if f_max is not None else img.max()
+    temp_img = 255 * (img - f_min) / (f_max - f_min)
+    return clip(temp_img , 0 , 255).astype(uint8)
+    
+def copute_histogram(img):
+    H = array([img[img == pixel].shape[0] for pixel in range(256)])
+    return H
+
+def histogram_equalization(img):
+    H = copute_histogram(img)
+    size  = prod(img.shape)
+    probabilities = H / size
+    C = [sum(probabilities[:i+1]) for i in range(256)]
+    return C
+
+def threshold(img , threshold):
+    temp = img >= threshold
+    return temp * 255 
+
+
+
+
+#py processing.py
